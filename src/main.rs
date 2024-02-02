950274369c39ca41316cce95bed9889b8acf4726
inyourface34456 <62214409+inyourface34456@users.noreply.github.com>
2024-02-02 18:37:27 +0000

    baisicly done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-#[macro_use] 
+#[macro_use]
 extern crate rocket;
 
-use rocket::fs::FileServer;
-use rocket::form::Form;
-use rocket::http::{ContentType, Status};
-use simple_crypt::{encrypt, decrypt};
 use base64::prelude::*;
+use rocket::form::Form;
+use rocket::fs::FileServer;
+use rocket::http::{ContentType, Status};
+use simple_crypt::{decrypt, encrypt};
 
 #[derive(FromForm)]
 struct Crypt {
     pub data: String,
-    pub password: String
+    pub password: String,
 }
 
 #[post("/encrypt", data = "<data>")]
@@ -20,11 +20,14 @@ fn encrypt_path(data: Form<Crypt>) -> (Status, (ContentType, String)) {
     let password = inner.password;
 
     match encrypt(data.as_bytes(), password.as_bytes()) {
-        Ok(dat) => return (Status::Ok, (ContentType::JSON, BASE64_STANDARD.encode(dat.as_slice()))),
-        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string()))
+        Ok(dat) => {
+            return (
+                Status::Ok,
+                (ContentType::JSON, BASE64_STANDARD.encode(dat.as_slice())),
+            )
+        }
+        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string())),
     }
-
-    
 }
 
 #[post("/decrypt", data = "<data>")]
@@ -35,27 +38,25 @@ fn decrypt_path(data: Form<Crypt>) -> (Status, (ContentType, String)) {
 
     let data = match BASE64_STANDARD.decode(data) {
         Ok(dat) => dat,
-        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string()))
+        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string())),
     };
 
     match decrypt(data.as_slice(), password.as_bytes()) {
         Ok(dat) => {
             let dat = match String::from_utf8(dat) {
                 Ok(dat) => dat,
-                Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string()))
+                Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string())),
             };
 
             return (Status::Ok, (ContentType::JSON, dat));
-        },
-        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string()))
+        }
+        Err(_) => return (Status::BadGateway, (ContentType::JSON, "".to_string())),
     }
-
-    
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![encrypt_path, decrypt_path])
-    .mount("/", FileServer::from("static"))
+        .mount("/", routes![encrypt_path, decrypt_path])
+        .mount("/", FileServer::from("static"))
 }
---
+++ static/decode/script.js
@@ -0,0 +1,38 @@
+let data = document.getElementById("data");
+let submit = document.getElementById("submit-data");
+let passwordBox = document.getElementById("password_box");
+let output = document.getElementById("output");
+
+function sendData(prams) {
+    fetch("/decrypt", {
+        method: "POST",
+        body: prams
+    }).then((responce) => {
+        if (responce.ok) {
+            return responce.text();
+        } else {
+            return responce.text();
+        }
+    }).then((data) => {
+        output.textContent = data;
+    })
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    sendData(new URLSearchParams({
+        data: data.value,
+        password: passwordBox.value
+    }));
+});
+
+
+document.addEventListener('keydown', (event)=> {    
+    if (event.key == "Enter") {
+        sendData(new URLSearchParams({
+            data: data.value,
+            password: passwordBox.value
+        }));
+    }
+});
